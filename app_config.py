eedfdb413a7872aaeb902101bc2c038cb2c13598
Melekh11 <cktkutxd@gmail.com>
2022-09-11 01:08:02 +0300

    all tables created

---
+++ app_config.py
@@ -0,0 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'gum_matinfo_luchshie17'
+    static_folder = '../build'
+    static_url_path = '/'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
